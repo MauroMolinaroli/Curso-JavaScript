cd7a5d473636ea543537319f270e118b0a29a3cf
unknown <molinarolimauro1@gmail.com>
2022-07-29 11:05:48 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,76 @@
+let nombre = "coder";
+let apellido = "house";
+let fechaNacimiento = "21/03/1975";
+const nombreCompleto = "joe mcmillian";
+let BLANCO = " ";
+
+let numeroA = 21;
+let numeroB = 3;
+let resultado = numeroA / numeroB;
+let empresa = nombre + " " + apellido;
+
+/*let confirmar = confirm("¿desea formatear su disco?")
+let datoIngresado = prompt("ingrese su nro. de cuenta bancaria:")
+let salida = datoIngresado + " " + "ingresada"
+alert(salida)
+*/
+
+
+//let username = prompt("ingrese su nombre:")
+//debugger
+
+//if (username == "mauro molinaroli") {
+//    console.log("bienvenido", username)
+
+//}
+//else {
+//    console.warn("no se reconoce el usuario")
+//}
+
+// let miOferta = prompt("ingrese su oferta")
+
+// if (miOferta < 22) {
+//     console.error("no seas raton")
+// }
+// else if (miOferta < 30) {
+//     console.warn("seguis siendo una rata")
+// }
+// else if (miOferta < 50) {
+//     console.warn("¿sos pobre?")
+// }
+// else {
+//     console.log("oferta aceptada")
+// } 
+
+// let username = prompt("ingrese su nombre de usuario")
+// let password = prompt("ingrese su contraseña")
+// let fingerPrint = confirm("¿desea utilizar su huella digital?")
+
+// if ((username == "mauro" && password == 123) || (fingerPrint == true)) {
+
+//     console.log("bienvenido", username)
+// }
+
+// else {
+//     console.warn("no se reconoce el usuario y/o contraseña.")
+// }
+
+// let nombreIngresado = prompt("ingresar nombre")
+
+// if (((nombreIngresado !="") && (nombreIngresado == "EMA")) || (nombreIngresado == "ema")) {
+//     console.log("bienvenido", nombreIngresado)
+// }
+
+// else {
+//     alert("error: ingresar nombre valido")
+// }
+
+
+// for (let i = 0; i <= 21; i++) {
+//     console.log("iteracion Nro.", i)
+// }
+
+let limite = parseInt(prompt("ingrese un numero"))
+
+for (let i = 0; i < limite; i++)
+    console.log("iteracion nro.", i)
